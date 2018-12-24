16d308ad0be72ee2e65152a328e0f04576dbd207
eric <erichappe@gmail.com>
2018-12-23 22:37:45 -0800

    initial migration commit

---
+++ assets/js/social.js
@@ -0,0 +1,4 @@
+/* Facebook*/
+window.fbAsyncInit = function(){ FB.init({ appId: '1500355830288223', xfbml: true, version: 'v2.4'});}; (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_EN/all.js#xfbml=1&status=0"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
+/* Twitter */
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
